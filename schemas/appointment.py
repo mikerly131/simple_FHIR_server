4f016b6f182dbc07a9d63ff27511b4a4cbc72118
mikerly131 <mrr5068@gmail.com>
2024-03-21 21:23:20 -0400

    appointment and slot p-models

--- schemas/appointment.py
+++ schemas/appointment.py
@@ -4,7 +4,9 @@ The pydantic model for appointment resource based on FHIR R4 specification.
 I'm choosing to implement a lot of the elements for learning purposes, examples show less are often used.
 """
 from pydantic import BaseModel
+from typing import Optional
 from enum import Enum
+from slot import Slot
 
 
 # Appointment status has defined set of possible values
@@ -27,32 +29,41 @@ class ParticipantStatus(str, Enum):
     needs_actions = 'needs-action'
 
 
+# Appointment participants are either required, optional or information-only
+class ParticipantRequired(str, Enum):
+    required = 'required'
+    optional = 'optional'
+    info_only = 'information-only'
+
+
 # Appointments require one or more participants - patient, providers, and/or locations
 class AppointmentParticipant(BaseModel):
-    type: str
-    actor: str
+    type: Optional[list] = None
+    actor: dict
     status: ParticipantStatus
+    required: ParticipantRequired
 
 
 class Appointment(BaseModel):
     resource_type: str
     id: str
-    identifier: list
+    text: Optional[dict] = None
+    identifier: Optional[list] = None
     status: AppointmentStatus
-    cancelation_reason: str | None = None
-    service_category: str
-    service_type: str
-    speciality: str
-    appointment_type: str
-    reason_code: str
-    reason_reference: str
-    priority: str
-    description: str
+    cancelation_reason: Optional[list] = None
+    service_category: Optional[list] = None
+    service_type: Optional[list] = None
+    specialty: Optional[list] = None
+    appointment_type: Optional[list] = None
+    reason_code: Optional[list] = None
+    reason_reference: Optional[list] = None
+    priority: Optional[int] = None
+    description: Optional[str] = None
     start: str
     end: str
-    minutes_duration: int
-    slot: str
+    minutes_duration: Optional[int] = None
+    slot: list[dict]
     created: str
-    comment: str
-    patient_instruction: str
-    participant: AppointmentParticipant
+    comment: Optional[str] = None
+    patient_instruction: Optional[str] = None
+    participant: list[AppointmentParticipant]
--- schemas/slot.py
+++ schemas/slot.py
@@ -1,25 +1,40 @@
 """
 The pydantic model for the slot resource based on FHIR R4 specification.
 
-Slots are time-slots for a specific schedule, and don't provide appointment information.
+Slots are time-slots for a specific schedule, and don't have to provide appointment information.
 Slots can have more than one appointment, but not in my system for now.
+serviceCategory, serviceType, speciality, appointmentType:
+- what slot can be used for, not appointment details in my system
 """
 from pydantic import BaseModel
+from typing import Optional
+from enum import Enum
+
+
+# Slots have a mandatory status
+class SlotStatus(str, Enum):
+    busy = 'busy'
+    free = 'free'
+    unavailable = 'busy-unavailable'
+    tentative = 'busy-tentative'
+    error = 'entered-in-error'
 
 
 class Slot(BaseModel):
     resource_type: str
-    identifier: str
-    service_category: str
-    service_type: str
-    specialty: str
-    appointment_type: str
-    schedule: str
-    status: str
+    id: str
+    text: Optional[dict] = None
+    identifier: Optional[list] = None
+    service_category: Optional[list] = None
+    service_type: Optional[list] = None
+    specialty: Optional[list] = None
+    appointment_type: Optional[list] = None
+    schedule: dict
+    status: SlotStatus
     start: str
     end: str
     overbooked: bool
-    comment: str
+    comment: Optional[str] = None
 
 
 
